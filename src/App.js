aec19f5b9688d08f180014f7a542e5968c3a94cf
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-06-28 21:20:50 -0700

    create basic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NavBar />
+      <Switch>
+        
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Nations.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nations() {
+    return(
+        <div>
+            <h2>Nations</h2>
+        </div>
+    );
+}
+
+export default Nations;
