d5711237fcf4f6f57766e8ff04c9a959b6c78eb4
Sana Mohiuddin <smohiuddin1688@gmail.com>
2023-03-26 20:46:06 -0700

    make some updates

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import CharacterItem from "./CharacterItem";
 import CharacterForm from "../CharacterForm";
 
-function Characters({characters, setCharacters}) {
-    const [showForm, setShowForm] = useState(false);
+function Characters({characters}) {
 
     const renderCharacters = characters.map(character => {
         return <CharacterItem
@@ -16,15 +15,9 @@ function Characters({characters, setCharacters}) {
         />
     });
 
-    function handleNewCharacterSubmit(newCharacter) {
-        setCharacters([...characters, newCharacter]);
-    }
-
     return(
         <div className="characterPage">
             <h1 id="characterHeader">Meet the Cast of Characters</h1>
-            <button onClick={() => setShowForm(!showForm)}>{showForm ? "Don't Add Character": "Add A Missing Character"}</button>
-            {showForm ? <CharacterForm onNewCharacterSubmit={handleNewCharacterSubmit} setShowForm={setShowForm} /> : null}
             {renderCharacters}
         </div>
     );
